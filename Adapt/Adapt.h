dbc5c3977e8106fdd359b039b00c2f2d8ace23b5
Dylan Reich <dylan@dylanreich.com>
2019-11-05 22:27:21 -0800

    Initial Commit

---
+++ Adapt/Adapt.h
@@ -0,0 +1,19 @@
+//
+//  Adapt.h
+//  Adapt
+//
+//  Created by Dylan Reich on 11/5/19.
+//  Copyright © 2019 Dylan Reich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Adapt.
+FOUNDATION_EXPORT double AdaptVersionNumber;
+
+//! Project version string for Adapt.
+FOUNDATION_EXPORT const unsigned char AdaptVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Adapt/PublicHeader.h>
+
+
