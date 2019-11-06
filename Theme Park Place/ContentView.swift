5a17c30513dcebcc6b7fa26f3effda9ffa9fa578
Dylan Reich <dylan@dylanreich.com>
2019-11-05 23:21:58 -0800

    added Adapt podspec

---
+++ Theme Park Place/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Theme Park Place
+//
+//  Created by Dylan Reich on 11/5/19.
+//  Copyright © 2019 Dylan Reich. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
